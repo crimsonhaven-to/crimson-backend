45fdbdea6186423661517325567eed3ffd64c948
4G0NYY <service@agony.ch>
2026-06-04 15:48:21 +0200

    Implement animesuge scraper

--- resolvers/__init__.py
+++ resolvers/__init__.py
@@ -2,7 +2,7 @@ from .vidmoly import VidmolyResolver
 from .voe import VoeResolver
 from .vidking import VidkingResolver
 from .animekai import AnimekaiResolver
-from .flix2day import DirectM3U8Resolver, AsbGamesResolver
+from .animesuge import DirectM3U8Resolver, AsbGamesResolver
 
 # The unified list of all our resolvers
 ALL_RESOLVERS = [
