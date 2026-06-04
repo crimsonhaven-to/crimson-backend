7f06a9614658f64b7b5f8d4a4504aec2bea7b570
4G0NYY <service@agony.ch>
2026-06-04 12:47:13 +0200

    Add CORS for local dev

--- api.py
+++ api.py
@@ -51,6 +51,8 @@ class Config:
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://127.0.0.1",
+        "http://127.0.0.1:8080",
+        "http://localhost:8080",
         #TODO: Add my production domains here
     ]
     
