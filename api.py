f11b0d7d374616482748bf77425cf3b19b71e279
4G0NYY <service@agony.ch>
2026-06-04 13:38:41 +0200

    pls work CORS

--- api.py
+++ api.py
@@ -53,9 +53,9 @@ class Config:
         "http://127.0.0.1",
         "http://127.0.0.1:8080",
         "http://localhost:8080",
-        "dev.crimsonhaven.to",
-        "crimsonhaven.to",
-        "www.crimsonhaven.to"
+        "https://dev.crimsonhaven.to",
+        "https://crimsonhaven.to",
+        "https://www.crimsonhaven.to"
     ]
     
     @classmethod
