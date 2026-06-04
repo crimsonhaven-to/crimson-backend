f12d8a65bad7b00c7ae821ac723a53469003301a
4G0NYY <service@agony.ch>
2026-06-04 15:34:17 +0200

    Add some more

--- resolvers/__init__.py
+++ resolvers/__init__.py
@@ -2,6 +2,7 @@ from .vidmoly import VidmolyResolver
 from .voe import VoeResolver
 from .vidking import VidkingResolver
 from .animekai import AnimekaiResolver
+from .flix2day import DirectM3U8Resolver, AsbGamesResolver
 
 # The unified list of all our resolvers
 ALL_RESOLVERS = [
@@ -9,4 +10,6 @@ ALL_RESOLVERS = [
     VoeResolver,
     VidkingResolver,
     AnimekaiResolver,
+    DirectM3U8Resolver,
+    AsbGamesResolver,
 ]
--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -3,10 +3,12 @@ from .gogo_scraper import GogoScraper
 from .voe_scraper import VoeScraper
 from .vidking_scraper import VidkingScraper
 from .animekai_scraper import AnimekaiScraper
+from .flix2day_scraper import Flix2dayScraper
 
 ALL_SCRAPERS = [
     GogoScraper,
     #VoeScraper,
     VidkingScraper,
     AnimekaiScraper,
+    Flix2dayScraper,
 ]
